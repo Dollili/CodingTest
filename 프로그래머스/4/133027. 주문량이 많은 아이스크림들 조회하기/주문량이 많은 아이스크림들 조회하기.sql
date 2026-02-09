c70b6b1791425ce3b9293eec99913e3d93dde931
Doil <102784266+Dollili@users.noreply.github.com>
2026-02-09 22:49:50 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,13 +1,11 @@
 -- 코드를 입력하세요
 select flavor
-from (select a.flavor, sum(tt + t) as total, row_number() over(order by sum(tt + t) desc) as rn
-from (select  flavor, sum(total_order) as tt
-      from first_half
-     group by flavor) a, (
-    select flavor, sum(total_order) as t
-    from july 
-    group by flavor       
-    ) b
-where a.flavor = b.flavor
-group by a.flavor) t
+from (select flavor, sum(total_order) as total, row_number() over(order by sum(total_order) desc) as rn
+      from (
+        select flavor, total_order from first_half
+        union all
+        select flavor, total_order from july
+      )
+      group by flavor
+      ) t
 where t.rn <= 3
