9c9528165654e6fb9b18c916779d56b389d83d44
Doil <102784266+Dollili@users.noreply.github.com>
2026-02-13 00:32:43 +0900

    [level 2] Title: 올바른 괄호, Time: 8.92 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,16 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        if (s.startsWith(")") || s.endsWith("(")) return false;
+        int cnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {cnt++;}
+            else {cnt--;}
+            if (cnt < 0) {
+                return false;
+            }
+        }       
+        
+        return cnt == 0;
+    }
+}
